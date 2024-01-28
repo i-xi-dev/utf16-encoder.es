c03e50d95103c42987c8d7ec3061a0efed6381f7
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-28 16:28:03 +0900

    2.0.3

--- deps.ts
+++ deps.ts
@@ -2,9 +2,9 @@ export {
   CodePoint,
   Rune,
   StringEx,
-} from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.2/mod.ts";
-export { TextEncoding } from "https://raw.githubusercontent.com/i-xi-dev/text-encoding.es/6.3.3/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.3/mod.ts";
+export { TextEncoding } from "https://raw.githubusercontent.com/i-xi-dev/text-encoding.es/6.3.4/mod.ts";
 export {
   Uint16,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.0.0/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.1.0/mod.ts";
